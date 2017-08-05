fb2b5ed47e48523a816b1875d6928110074e049e
Jose Fco Moreno Fenandez <josefcomoreno85@gmail.com>
2017-08-06 00:50:30 +0200

    Login1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,6 +49,8 @@ export class App {
   private _loadImages(): void {
     // register some loaders
     BaThemePreloader.registerLoader(this._imageLoader.load('/assets/img/sky-bg.jpg'));
+    //BaThemePreloader.registerLoader(this._imageLoader.load('/assets/img/fondoLogin.png'));
   }
 
 }
+
